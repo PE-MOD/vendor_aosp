7491b78eb64d14a295ef3b7cfd87273d8489315f
ezio84 <brabus84@gmail.com>
2019-11-17 16:47:26 +0100

    Colorpicker: allow to use it with xml calls only and use android:defaultValue
    Custom prefs: don't crash if we forgot to add android:defaultValue
    
    Change-Id: Iffc5eca199ae5e85ccb37e98e31d56b7e542f21b
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>
    Signed-off-by: kunal <kunalshah912@gmail.com>

--- support/src/com/pe/support/preference/CustomSeekBarPreference.java
+++ support/src/com/pe/support/preference/CustomSeekBarPreference.java
@@ -265,20 +265,15 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
     }
 
     @Override
-    protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
-        if (restoreValue) {
-            mCurrentValue = getPersistedInt(mCurrentValue);
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        // when using PreferenceDataStore, restorePersistedValue is always true (see Preference class for reference)
+        // so we load the persistent value with getPersistedInt if available in the data store, 
+        // and use defaultValue as fallback (onGetDefaultValue has been already called and it loaded the android:defaultValue attr from our xml).
+        if (defaultValue == null) {
+            // if we forgot to add android:defaultValue, default to 0
+            defaultValue = 0;
         }
-        else {
-            int temp = 0;
-            try {
-                temp = (Integer) defaultValue;
-            } catch (Exception ex) {
-                Log.e(TAG, "Invalid default value: " + defaultValue.toString());
-            }
-            persistInt(temp);
-            mCurrentValue = temp;
-        }
+        mCurrentValue = getPersistedInt((Integer) defaultValue);
     }
 
     public void setDefaultValue(int value) {
