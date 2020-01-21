DerpLab/platform_vendor_aosip

vendor: Add a bunch of accents for ThemePicker
Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

config/common.mk
@@ -116,6 +116,9 @@ include vendor/aosip/config/fonts.mk
 # Packages
 include vendor/aosip/config/packages.mk
 
+# Themes
+include vendor/aosip/config/accents.mk
+
 # Versioning
 include vendor/aosip/config/version.mk
 

PE-MOD/vendor_aosp

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Change-Id: Id6b86f7057597874d15c6a18364098fd3557abbc
Signed-off-by: kunal <kunalshah912@gmail.com>

config/common.mk
@@ -147,3 +147,6 @@ include vendor/google/customization/config.mk
 
 # Packages
 include vendor/aosp/config/packages.mk
+
+# Themes
+include vendor/aosp/config/accents.mk